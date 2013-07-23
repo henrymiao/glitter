52e09965d44073ccc7155045a7b42bd73f3b6382
shuren <shuren@192.168.92.63>
2013-07-23 14:20:59 +0800

    bootstrap

---
+++ remote-example/src/main/java/com/yihaodian/architecture/remote/example/service/BootStrap.java
@@ -0,0 +1,13 @@
+package com.yihaodian.architecture.remote.example.service;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BootStrap {
+	
+	private BootStrap()
+	{}
+	public static void main(String[] args) {
+			new ClassPathXmlApplicationContext(
+	            new String[] { "applicationContext.xml"});
+	}		
+}
