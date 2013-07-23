558d9318407da301ee8940e28c9095e1721ec5f0
Archer <aaaajl@gmail.com>
2013-07-23 15:38:02 +0800

    change interface

--- remote-example/src/main/java/com/yihaodian/architecture/remote/example/service/impl/ExampleServiceImpl.java
+++ remote-example/src/main/java/com/yihaodian/architecture/remote/example/service/impl/ExampleServiceImpl.java
@@ -2,17 +2,18 @@ package com.yihaodian.architecture.remote.example.service.impl;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import com.yihaodian.architecture.remote.example.service.DataCreator;
 import com.yihaodian.architecture.remote.example.service.ExampleService;
 import com.yihaodian.architecture.remote.example.service.ParameterVal;
 import com.yihaodian.architecture.remote.example.service.ResultObject;
 
 public class ExampleServiceImpl implements ExampleService {
 
-	private AtomicInteger count=new AtomicInteger(0);
-	
+	private AtomicInteger count = new AtomicInteger(0);
+
 	public ResultObject testService(String name, ParameterVal parameter) {
 		// TODO Auto-generated method stub
-		System.out.println(name+"*******");
+		System.out.println(name + "*******");
 		return new ResultObject("return");
 	}
 
@@ -26,9 +27,10 @@ public class ExampleServiceImpl implements ExampleService {
 		System.out.println("no return other result");
 	}
 
-	public void testDataService(String name) {
+	public String testDataService(String name) {
 		// TODO Auto-generated method stub
-		System.out.println("recevied Data"+count.incrementAndGet());
+		System.out.println("recevied Data" + count.incrementAndGet());
+		return DataCreator.createObject(1);
 	}
 
 }
--- remote-example/src/test/java/com/yihaodian/architecture/remote/example/test/service/ExampleClientTest.java
+++ remote-example/src/test/java/com/yihaodian/architecture/remote/example/test/service/ExampleClientTest.java
@@ -1,7 +1,5 @@
 package com.yihaodian.architecture.remote.example.test.service;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,45 +11,46 @@ public class ExampleClientTest extends AbstractServiceTest {
 
 	@Autowired
 	public ExampleService exampleClient;
-	
+
 	@Test
-	public void TestExampleService() throws InterruptedException
-	{
-		for(int i=0;i<10;i++)
-		{
-		 exampleClient.testService();
+	public void TestExampleService() throws InterruptedException {
+		for (int i = 0; i < 10; i++) {
+			exampleClient.testService();
 		}
-		
+
 		Thread.sleep(100000);
 	}
+
 	@Test
-	public void TestExampleOtherService() throws InterruptedException
-	{
+	public void TestExampleOtherService() throws InterruptedException {
 		exampleClient.testOtherService();
 		Thread.sleep(1000);
 	}
+
 	@Test
-	public void TestDataSize() throws Exception
-	{
-		//AtomicInteger count=new AtomicInteger(0);
-		Long curr=System.currentTimeMillis();
-		for(int i=0;i<50000;i++)
-		{
+	public void TestDataSize() throws Exception {
+		// AtomicInteger count=new AtomicInteger(0);
+		for (int i = 0; i < 50000; i++) {
 			exampleClient.testDataService(DataCreator.createObject(2));
 			System.out.println(i);
 		}
-		System.out.println(System.currentTimeMillis()-curr);
+		Long curr = System.currentTimeMillis();
+		for (int i = 0; i < 50000; i++) {
+			String a = exampleClient.testDataService(DataCreator.createObject(2));
+			System.out.println(i);
+		}
+		System.out.println(System.currentTimeMillis() - curr);
 		Thread.sleep(1000000);
 	}
+
 	@Test
-	public void TestExampleService3()
-	{
-		for(int i=0;i<100;i++)
-		{
-		String name="helloworld";
-		ParameterVal parameter=new ParameterVal("start");
-		exampleClient.testService(name, parameter);
-		//System.out.println(exampleClient.testService(name, parameter).getValue());
+	public void TestExampleService3() {
+		for (int i = 0; i < 100; i++) {
+			String name = "helloworld";
+			ParameterVal parameter = new ParameterVal("start");
+			exampleClient.testService(name, parameter);
+			// System.out.println(exampleClient.testService(name,
+			// parameter).getValue());
 		}
 		try {
 			Thread.sleep(100000);
