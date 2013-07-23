f34fa123df26da8faab26dbb889e8776b5b20d58
shuren <shuren@192.168.92.63>
2013-07-23 14:22:41 +0800

    sds

--- remote-example/src/main/java/com/yihaodian/architecture/remote/example/service/BootStrap.java
+++ remote-example/src/main/java/com/yihaodian/architecture/remote/example/service/BootStrap.java
@@ -5,7 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class BootStrap {
 	
 	private BootStrap()
-	{}
+	{
+		
+	}
 	public static void main(String[] args) {
 			new ClassPathXmlApplicationContext(
 	            new String[] { "applicationContext.xml"});
